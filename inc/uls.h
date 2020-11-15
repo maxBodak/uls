54d185485270d6a1a8e98b11709bcbfcc39d46ab
maxBodak <mbodak@e1r10p7.unit.ua>
2020-11-15 21:46:52 +0200

    1

---
+++ inc/uls.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "libmx.h"
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+#define INT_MAX 2147483647
+
+
+enum flags {
+    R,
+    l,
+    one,
+    m,
+    C,
+    S,
+    t,
+    r,
+    p,
+    u,
+    c,
+    dog,
+    e,
+    h,
+    T
+};
+
+typedef struct s_path {
+    char **path;
+    int amt;
+}              t_path;
